4da43f764f41f34afb43905faa6a68f47e9c8f84
RikkiRun <31293953+RikkiRun@users.noreply.github.com>
2018-08-25 17:38:04 -0600

    Add check status

--- ShaderDevPractice/MyGlWindow.cpp
+++ ShaderDevPractice/MyGlWindow.cpp
@@ -1,26 +1,24 @@
 #include <gl\glew.h>
+#include <iostream>
 #include <MyGlWindow.h>
+using namespace std;
 
 extern const char* vertexShaderCode;
 extern const char* fragmentShaderCode;
 
 void sendDataToOpenGL() {
 	GLfloat verts[] = {
-	+0.0f, +0.0f,   //position
+	+0.0f, +1.0f,   //position
 	+1.0f, +0.0f, +0.0f, // color
-	+1.0f, +1.0f,
-	+1.0f, +0.0f, +0.0f,
-	-1.0f, +1.0f,
-	+1.0f, +0.0f, +0.0f,
 	-1.0f, -1.0f,
-	+1.0f, +0.0f, +0.0f,
+	+0.0f, +0.0f, +1.0f,
 	+1.0f, -1.0f,
-	+1.0f, +0.0f, +0.0f,
+	+0.0f, +1.0f, +0.0f,
 	};
 	GLuint vertexBufferID; // vertex bufferID
 
 	glGenBuffers(1, &vertexBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID); 
 	glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
 
 	glEnableVertexAttribArray(0); //position
@@ -30,11 +28,78 @@ void sendDataToOpenGL() {
 	glEnableVertexAttribArray(1); //color
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 5, (char*)(sizeof(float) * 2)); // last one: 2 float until we get to the beginning of the color data
 
-	GLushort indices[] = { 0, 1, 2, 0, 3, 4 }; // save memory
+	GLushort indices[] = { 0, 1, 2}; // save memory
 	GLuint indexBufferID; // element bufferID
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+}
+
+bool checkStatus(GLint objectID, 
+	PFNGLGETSHADERIVPROC objectPropertyGetterFunc, 
+	PFNGLGETSHADERINFOLOGPROC getInfoLogFunc, 
+	GLenum statusType){
+
+	GLint status; // only 1 status
+	objectPropertyGetterFunc(objectID, statusType, &status);
+	if (status != GL_TRUE) {
+		GLint infoLogLength;
+		objectPropertyGetterFunc(objectID, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar* buffer = new GLchar[infoLogLength];
+
+		GLsizei bufferSize;
+		getInfoLogFunc(objectID, infoLogLength, &bufferSize, buffer);
+		cout << buffer << endl;
+
+		delete[] buffer;
+		return false;
+	}
+	return true;
+
+}
+
+bool checkShaderStatus(GLuint shaderID) {
+	/*
+	//get GLSL compiler error
+	GLint compilerStatus; // only 1 status
+	glGetShaderiv(shaderID, GL_COMPILE_STATUS, &compilerStatus);
+	if (compilerStatus != GL_TRUE) {
+		GLint infoLogLength;
+		glGetShaderiv(shaderID, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar* buffer = new GLchar[infoLogLength];
+
+		GLsizei bufferSize;
+		glGetShaderInfoLog(shaderID, infoLogLength, &bufferSize, buffer);
+		cout << buffer << endl;
+
+		delete[] buffer;
+		return false;
+	}
+	return true;
+	*/
+
+	return checkStatus(shaderID, glGetShaderiv, glGetShaderInfoLog, GL_COMPILE_STATUS);
+}
+
+bool checkProgramStatus(GLuint programID) {
+	/*
+	GLint linkStatus;
+	glGetProgramiv(programID, GL_LINK_STATUS, &linkStatus);
+	if (linkStatus != GL_TRUE) {
+		GLint infoLogLength;
+		glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &infoLogLength);
+		GLchar* buffer = new GLchar[infoLogLength];
+
+		GLsizei bufferSize;
+		glGetProgramInfoLog(programID, infoLogLength, &bufferSize, buffer);
+		cout << buffer << endl;
+		delete[] buffer;
+		return false;
+	}
+	return true;
+	*/
+	return checkStatus(programID, glGetProgramiv, glGetProgramInfoLog, GL_LINK_STATUS);
 
 }
 
@@ -50,11 +115,19 @@ void installShaders() {
 	
 	glCompileShader(vertexShaderID);
 	glCompileShader(fragmentShaderID);
+	
+	if (!checkShaderStatus(vertexShaderID) || !checkShaderStatus(fragmentShaderID)) {
+		return;
+	}
 
 	GLuint programID = glCreateProgram();
 	glAttachShader(programID, vertexShaderID);
 	glAttachShader(programID, fragmentShaderID);
 	glLinkProgram(programID);
+
+	if (!checkProgramStatus(programID)) {
+		return;
+	}
 
 	glUseProgram(programID);
 }
--- ShaderDevPractice/MyShaderCode.cpp
+++ ShaderDevPractice/MyShaderCode.cpp
@@ -1,21 +1,26 @@
 const char* vertexShaderCode =
 "#version 430\r\n" //declare what vision 
 ""
-"in layout(location = 0) vec2 position" //vec2: for attribute 0, 2 floats for position
+"in layout(location = 0) vec2 position;" //vec2: for attribute 0, 2 floats for position
+"in layout(location = 1) vec3 vertexColor;"
+""
+"out vec3 theColor;" // send to the fragmentShader
 ""
 "void main()"
 "{"
 "	gl_Position = vec4(position, 0.0, 1.0);"
+"	theColor = vertexColor;"
 "}";
 
 
 const char* fragmentShaderCode =
 "#version 430\r\n"
 ""
-"out vec4 daColor"
+"out vec4 daColor;"
+"in vec3 theColor;" // from vertex shader
 ""
 "void main()"
 "{"
-"	daColor = vec4(0.0, 1.0, 0.0, 1.0);"
+"	daColor = vec4(theColor, 1.0);"
 "}";
 
