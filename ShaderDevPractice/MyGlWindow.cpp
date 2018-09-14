ddf94d42654f4e660c82a235cc7a0dd69343388d
RikkiRun <31293953+RikkiRun@users.noreply.github.com>
2018-09-13 20:42:48 -0600

    add uniform movement

--- ShaderDevPractice/MyGlWindow.cpp
+++ ShaderDevPractice/MyGlWindow.cpp
@@ -7,106 +7,68 @@ using namespace std;
 extern const char* vertexShaderCode;
 extern const char* fragmentShaderCode;
 
-const float X_DELTA = 0.1f;
-const uint NUM_VERTICES_PER_TRI = 3;
-const uint NUM_FLOAT_PER_VERTICE = 6;
-const uint TRIANGLE_BYTE_SIZE = NUM_VERTICES_PER_TRI * NUM_FLOAT_PER_VERTICE * sizeof(float);
-const uint MAX_TRIS = 20;
-
-uint numTris = 0;
-
 void sendDataToOpenGL() {
-	
-	//draw two triangles
-	const float RED_TRIANGLE_Z = -0.5f;
-	const float BLUE_TRIANGLE_Z = 0.5f;
-	const float TRIANGLE_Z = 0.5f;
 
 
-	/*
-	GLfloat verts[] = {
-	+0.0f, +1.0f, RED_TRIANGLE_Z,  //position
-	+1.0f, +0.0f, +0.0f, // color
-	-1.0f, -1.0f, RED_TRIANGLE_Z,
-	+1.0f, +0.0f, +0.0f,
-	+1.0f, -1.0f, RED_TRIANGLE_Z,
-	+1.0f, +0.0f, +0.0f,
+	GLfloat theTri[] = {
+	-1.0f, 1.0f, 0.0f,
+	0.6f, 0.0f, 0.8f,
 
-	+0.0f, -1.0f, BLUE_TRIANGLE_Z,  //position
-	+1.0f, +0.0f, +1.0f, // color
-	+1.0f, +1.0f, BLUE_TRIANGLE_Z,
-	+1.0f, +0.0f, +1.0f,
-	-1.0f, +1.0f, BLUE_TRIANGLE_Z,
-	+1.0f, +0.0f, +1.0f,
+	-1.0f, 0.9f, 0.0f,
+	0.8f, 0.0f, 0.2f,
+
+	-0.9f, 0.9f, 0.0f,
+	0.5f, 0.0f, 0.7f,
 	};
-	*/
-	
 
-	/*
-	GLfloat verts[] = {
-		+0.0f, +0.0f, TRIANGLE_Z
-		+1.0f, +0.0f, +0.0f,
-
-		+0.0f, -0.4f, TRIANGLE_Z
-		+1.0f, +0.0f, +0.0f,
-
-		+0.3f, +0.0f, TRIANGLE_Z
-		+1.0f, +0.0f, +0.0f,
-
-		+0.3f, -0.3f, TRIANGLE_Z
-		+1.0f, +0.0f, +0.0f
-	};
-	*/
-	
 
 	GLuint vertexBufferID; // vertex bufferID
 
 	glGenBuffers(1, &vertexBufferID);
-	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID); 
-	//glBufferData(GL_ARRAY_BUFFER, sizeof(verts), verts, GL_STATIC_DRAW);
-	glBufferData(GL_ARRAY_BUFFER, MAX_TRIS * TRIANGLE_BYTE_SIZE, NULL, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexBufferID);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(theTri), theTri, GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, MAX_TRIS * TRIANGLE_BYTE_SIZE, NULL, GL_STATIC_DRAW);
 	glEnableVertexAttribArray(0); //position
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0); //GL_FALSE: Not normalize data; stride: where the data begins, the first position to the second position
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, 0); //GL_FALSE: Not normalize data; stride: where the data begins, the first position to the second position
 
-	// describe color attribute
+	// describe color attribute1
 	glEnableVertexAttribArray(1); //color
 	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(float) * 6, (char*)(sizeof(float) * 3)); // last one: 2 float until we get to the beginning of the color data
 
 
 	//indices
-	GLushort indices[] = { 0, 1, 2 , 1, 2, 3 }; // glushort for saving memory
+	GLushort indices[] = { 0, 1, 2 }; // glushort for saving memory
 	GLuint indexBufferID; // element bufferID
 	glGenBuffers(1, &indexBufferID);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexBufferID);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
-	
 }
 
-
+/*
 void sendAnotherTriToOpenGL() {
-	if (numTris == MAX_TRIS)
-		return;
-	const GLfloat THIS_TRI_X = -1 + numTris * X_DELTA;
+
 	GLfloat thisTri[] = {
-		THIS_TRI_X, 1.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
+		1.0f, 1.0f, 0.0f,
+		0.6f, 0.0f, 0.8f,
 
-		THIS_TRI_X + X_DELTA, 1.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
+		 0.0f, 1.0f, 0.0f,
+		0.8f, 0.0f, 0.2f,
 
-		THIS_TRI_X, 0.0f, 0.0f,
-		1.0f, 0.0f, 0.0f,
+		 -1.0f, 0.0f, 0.0f,
+		0.5f, 0.0f, 0.7f,
 	};
-	glBufferSubData(GL_ARRAY_BUFFER, 
+
+	glBufferSubData(GL_ARRAY_BUFFER,
 		TRIANGLE_BYTE_SIZE * numTris, TRIANGLE_BYTE_SIZE, thisTri);
 	numTris++;
 }
+*/
 
 
-bool checkStatus(GLint objectID, 
-	PFNGLGETSHADERIVPROC objectPropertyGetterFunc, 
-	PFNGLGETSHADERINFOLOGPROC getInfoLogFunc, 
-	GLenum statusType){
+bool checkStatus(GLint objectID,
+	PFNGLGETSHADERIVPROC objectPropertyGetterFunc,
+	PFNGLGETSHADERINFOLOGPROC getInfoLogFunc,
+	GLenum statusType) {
 
 	GLint status; // only 1 status
 	objectPropertyGetterFunc(objectID, statusType, &status);
@@ -176,7 +138,7 @@ string  readShaderCode(const char* fileName) {
 		cout << "File failed to load ..." << fileName;
 		exit(1);
 	}
-	 
+
 	return std::string(
 		std::istreambuf_iterator<char>(meInput),
 		std::istreambuf_iterator<char>()
@@ -194,10 +156,10 @@ void installShaders() {
 	temp = readShaderCode("FragmentShaderCode.glsl");
 	adapter[0] = temp.c_str();
 	glShaderSource(fragmentShaderID, 1, adapter, 0);
-	
+
 	glCompileShader(vertexShaderID);
 	glCompileShader(fragmentShaderID);
-	
+
 	if (!checkShaderStatus(vertexShaderID) || !checkShaderStatus(fragmentShaderID)) {
 		return;
 	}
@@ -229,13 +191,15 @@ void MyGlWindow::paintGL() {
 	//glClear(GL_DEPTH_BUFFER_BIT);
 	//glClear(GL_COLOR_BUFFER_BIT); //there's a back buffer and a front buffer, if not clear, it will switch
 	glViewport(0, 0, width(), height());
-	//glDrawArrays(GL_TRIANGLES, 0, 6);
+	//glDrawArrays(GL_TRIANGLES, 0, 3);
 	//draw a triangle
-	//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_SHORT, 0);
-	
-	
-	//draw a new triangle every new frame
+	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, 0);
+}
+/*
+//draw a new triangle every new frame
 	sendAnotherTriToOpenGL();
-	glDrawArrays(GL_TRIANGLES, (numTris - 1) * NUM_VERTICES_PER_TRI, 
+	glDrawArrays(GL_TRIANGLES, (numTris - 1) * NUM_VERTICES_PER_TRI,
 		numTris * NUM_VERTICES_PER_TRI);
 	}
+	*/
+	//uniform position --> keyboard movement 
--- ShaderDevPractice/VertexShaderCode.glsl
+++ ShaderDevPractice/VertexShaderCode.glsl
@@ -4,11 +4,17 @@
 in layout(location = 0) vec3 position; 
 in layout(location = 1) vec3 vertexColor;
 
+uniform float xMove;
+uniform float yMove;
+
 out vec3 theColor;
 
 void main()
 {
 	gl_Position = vec4(position, 1.0);
-	theColor = vertexColor;
-}
+	gl_Position.x = gl_Position.x + xMove;
+	gl_Position.y = gl_Position.y + yMove;
 
+	theColor = vertexColor;
+
+}
